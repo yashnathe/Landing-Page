6303a35e52c0111cbaa482b6ea76666cd011e447
yashnathe <83203499+yashnathe@users.noreply.github.com>
2023-02-07 19:17:22 +0530

    Update Footer.jsx

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
         </div>
       </footer>
       <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+        <span>© 2023 RentUP. Designd By YashNathe.</span>
       </div>
     </>
   )
